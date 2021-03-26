ab689cc5b71aefe862a0e25e91b387d0528f932e
TorrentialFire <wilsontfwi@gmail.com>
2021-03-26 11:28:23 -0500

    🎉 Initial commit, project setup

---
+++ include/tfwi_vulkan_gfx.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+#include "tfwi_vulkan_gfx_config.hpp"
---
+++ source/tfwi_vulkan_gfx.cpp
@@ -0,0 +1,15 @@
+#include "tfwi_vulkan_gfx.hpp"
+
+int main(int argc, char** argv) {
+
+	std::cout 
+		<< "LearnVulkan Version "
+		<< LEARN_VULKAN_VERSION_MAJOR
+		<< "."
+		<< LEARN_VULKAN_VERSION_MINOR
+		<< "."
+		<< LEARN_VULKAN_VERSION_PATCH
+		<< "\n";
+
+	return 0;
+}
